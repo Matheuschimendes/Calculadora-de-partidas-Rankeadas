7ec627f092e14378245d46ae69680d4bdd46ab71
Curso de JS <matheuschiimenndes@gmail.com>
2023-11-24 15:26:05 -0300

    Foram feitas interações com a interface do usuário

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 2️⃣ Desafio Calculadora de partidas Rankeadas
 
-
-
 **O Que deve ser utilizado**
 
 - Variáveis
@@ -22,6 +20,18 @@ depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve s
 - Se vitórias for entre 81 e 90 = Diamante
 - Se vitórias for entre 91 e 100= Lendário
 - Se vitórias for maior ou igual a 101 = Imortal
+
+## O que foi incluido
+
+Foram feitas interações com a interface do usuário, assim fazendo com que o próprio insira os pontos adquiridos da vitórias e os da derrota, com isso retornando seus resultados.
+
+    Elementos do DOM: Foram adicionadas referências aos elementos HTML usando document.getElementById para capturar valores de entrada do usuário e para exibir o resultado.
+
+    Atribuição de Valores dos Inputs: A função calcularRankeada agora utiliza document.getElementById("vitorias").value e document.getElementById("derrotas").value para obter os valores inseridos pelo usuário nos campos de entrada com IDs "vitorias" e "derrotas", respectivamente.
+
+    Atualização do Resultado na Página: Em vez de exibir o resultado no console, o saldo de vitórias e o nível alcançado são exibidos no HTML usando document.getElementById("resultado").innerHTML.
+
+    Retorno de Valores: A função continua retornando um objeto contendo o saldo de vitórias e o nível alcançado.
 
 ## Saída
 
--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
 // Exemplo de utilização da função
-let resultado = calcularRankeada(200, 100);
 
-function calcularRankeada(vitorias, derrotas){
-    let saldoVitorias = vitorias - derrotas
-    let nivel = ''
+//A função interage com o Document Object Model (DOM) para obter valores de elementos HTML. Ela busca valores de campos de entrada com os IDs "vitorias" e "derrotas" usando document.getElementById("ID") e os converte para números inteiros usando parseInt().
+function calcularRankeada(){
 
+    let vitorias = parseInt(document.getElementById("vitorias").value)
+    let derrotas = parseInt(document.getElementById("derrotas").value)
+
+    //calcula o saldo de vitórias subtraindo o número de derrotas do número de vitórias.
+    let saldoVitorias = document.getElementById("resultado").innerHTML = vitorias - derrotas
+
+    let nivel = '' 
+
+    //Usando estruturas condicionais (if...else if...else), a função verifica em qual faixa de quantidade de vitórias o jogador se encaixa e atribui um nível correspondente (Ferro, Bronze, Prata, etc.) à variável nivel.
     if( vitorias < 10){
         nivel = "Ferro"
-    }else if( vitorias >= 11 && vitorias <= 20){
+    }else if( vitorias>= 11 && vitorias <= 20){
         nivel = "Bronze"
     }else if( vitorias >= 21 && vitorias <= 50){
         nivel = "Prata"
@@ -22,9 +29,10 @@ function calcularRankeada(vitorias, derrotas){
         nivel = "Imortal"
     }
 
-
-    console.log(`O Herói tem saldo de ${saldoVitorias} está no nível ${nivel}`)
-    return {saldoVitorias, nivel}
+    //O resultado, composto pelo saldo de vitórias e o nível determinado, é exibido no HTML. Ele é atribuído ao elemento com o ID "resultado" usando document.getElementById("resultado").innerHTML.
+    document.getElementById("resultado").innerHTML = `O Herói tem saldo de ${saldoVitorias} está no nível ${nivel}`
+    
+    return {saldoVitorias, nivel} // A função retorna um objeto contendo o saldo de vitórias e o nível alcançado.
 }
 
 
